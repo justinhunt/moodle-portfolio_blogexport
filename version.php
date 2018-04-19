ca7405b7ba9019f0d143f593b2f8e39f63bff42a
justinhunt <bitwalkerjapan@gmail.com>
2018-04-19 16:53:15 +0900

    GDPR_updates

--- version.php
+++ version.php
@@ -23,9 +23,8 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2014062000;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018041000;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2012062500;        // Requires this Moodle version.
 $plugin->component = 'portfolio_blogexport'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.2 (Build 2014062000)';
-$plugin->cron      = 0;
+$plugin->release = '1.1.0 (Build 2018041000)';
